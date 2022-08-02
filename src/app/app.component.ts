e943bcd1dfca4885f53dca51018d1a5819c360cc
TheRealYaseen <s.syedyaseen.s@gmail.com>
2022-08-02 15:04:19 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {logMessages} from "@angular-devkit/build-angular/src/builders/browser-esbuild/esbuild";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'SocialMediaFE';
+export class AppComponent implements OnInit{
+
+  users: any;
+
+  constructor(private http: HttpClient) {  }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5000/users').subscribe({
+      next: value => this.users = value,
+      error: err => console.log(err.message)
+    })
+  }
 }
